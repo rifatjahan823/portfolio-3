5291959a0e09efedfed39279429af3477df1b227
rifatjahan823 <rifatjahan823@gmail.com>
2022-06-01 23:47:05 +0600

    complete banner

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import bg from '../../img/bg.jpg'
+import './Banner.css';
+import avatar from '../../img/avatar.png';
+import ps from '../../img/Adobe-Photoshop-2020-Logos-600x315-removebg-preview.png';
+import fg from '../../img/1_c2x4lhBVMM87YA1bhuhf7g.png';
+import ad from '../../img/images.png'
 
 const Banner = () => {
     return (
-        <div className='container' >
-           <div style={{}}>
-
+        <div style={{backgroundImage:`url(${bg})`, backgroundPosition:"50%",backgroundSize:"cover",backgroundRepeat:"no-repeat",padding:'100px 0',top:"0",bottom:'0',left:'0',right:"0",marginTop:"-82px"}}>
+            <div className='container' >
+                <div className='row g-4 align-items-center'>
+                    <div className='col-md-7 col-12 pe-5'>
+                        <div className='content-part mt-3'>
+                         <h3>Hello, I'm</h3>
+                         <h2>Rifat Jahan</h2>
+                         <p className='my-3'>A <span style={{color:'#1cbe59'}}>Creative Designer </span>From <span style={{color:"#8067f0"}}>Bangladesh</span></p>
+                         <p className='p-description mb-4'>I'm creative designer based in Bangladesh, and I'm very passionate and <br></br> dedicated to my work. </p>
+                         <div className='dizme_tm_button '>
+                            <a href="#about"><span>About Me</span></a>
+                         </div>
+                        </div>
+                    </div>
+                    <div className='col-md-5 mt-5 col-12'>
+                        <div className='img-part'>
+                            <img style={{width:'330px',}} className='img-fluid mt-5  ' src={avatar} alt="" />
+                            <img className='img-fluid animated-img ' src={ps} alt="" />
+                            <img className='img-fluid animated-img2 ' src={fg}alt="" />
+                            <img className='img-fluid animated-img3 ' src={ad} alt="" />
+                        </div>
+                    </div>
+                </div>
            </div>
         </div>
     );
--- src/Component/Navebar/Navebar.js
+++ src/Component/Navebar/Navebar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import logo from '../../img/logo.png'
+import './Navebar.css'
 
 const Navebar = () => {
     const [navBackground, setNavBackground] = useState(false)
@@ -8,7 +10,7 @@ const Navebar = () => {
     navRef.current = navBackground
     useEffect(() => {
       const handleScroll = () => {
-        const show = window.scrollY > 50
+        const show = window.scrollY >10
         if (navRef.current !== show) {
           setNavBackground(show)
         }
@@ -20,17 +22,40 @@ const Navebar = () => {
     }, [])
     return (
       <>
-       <Navbar fixed="top" style={{ transition: '1s ease',backgroundColor: navBackground ? 'red' : 'transparent'}}>
-        <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/about">About</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        </Container>
-      </Navbar>
-    
+      <div className='d-none d-lg-block'>
+      <Navbar collapseOnSelect expand="lg" fixed="top"style={{ transition: '1s ease',backgroundColor: navBackground ? 'white' : 'transparent',padding:"15px 0"}}>
+          <Container> 
+            <Navbar.Brand as={Link} to="/">
+           <img className='img-fluid dizme-img' src={logo} alt="" />
+         </Navbar.Brand>
+         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+             <Nav className="ms-auto">
+             <Nav.Link as={Link} to="/">Home</Nav.Link>
+               <Nav.Link as={Link} to="/about">About</Nav.Link>
+               <Nav.Link as={Link} to="/portfolio">My Portfolio</Nav.Link>  
+             </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className='d-block d-lg-none'>
+      <Navbar collapseOnSelect expand="lg" sticky="top" className='bg'>
+          <Container> 
+            <Navbar.Brand as={Link} to="/">
+           <img className='img-fluid dizme-img' src={logo} alt="" />
+         </Navbar.Brand>
+         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+             <Nav className="ms-auto">
+             <Nav.Link as={Link} to="/">Home</Nav.Link>
+               <Nav.Link as={Link} to="/about">About</Nav.Link>
+               <Nav.Link as={Link} to="/portfolio">My Portfolio</Nav.Link>  
+             </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     </>
   
     );
